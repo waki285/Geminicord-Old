7bc133330700bd16d6574258e17d463ed2d3b87d
waki285 <suzuneu@suzuneu.com>
2023-12-16 14:07:34 +0900

    musi

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const client = new Client({
 });
 const axios = require("axios");
 const { JSONParser } = require("@streamparser/json");
+const { EmbedBuilder } = require("discord.js");
 
 client.on("ready", () => {
   console.log(`${client.user.tag} has logged in.`);
@@ -14,6 +15,7 @@ client.on("ready", () => {
 client.on("messageCreate", async (message) => {
   if (message.author.bot) return;
   if (message.channel?.topic?.includes("Gemini")) {
+    if (message.content.startsWith("#")) return;
     const content = message.content.trim();
     const msg = await message.reply("Generating...");
     const res = await axios.post(
@@ -39,11 +41,13 @@ client.on("messageCreate", async (message) => {
     });
 
     const to = setInterval(() => {
-      msg.edit(text);
+      msg.edit(text.slice(0,1999));
     }, 3000);
 
     res.data.on("end", () => {
-      msg.edit(text);
+      const embed = new EmbedBuilder()
+        .setDescription("2000文字を超えるため、一部のみ表示しています。")
+      msg.edit({ content: text.slice(0,1999), embeds: [text.length > 1999 ? embed:[]].flat() });
       clearInterval(to);
     });
   }
