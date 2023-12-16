fc58f3974c7d6f427a1e71eab29fee45ec0d8fa0
waki285 <suzuneu@suzuneu.com>
2023-12-16 14:21:41 +0900

    a

--- src/index.js
+++ src/index.js
@@ -4,52 +4,60 @@ const { Client, GatewayIntentBits } = require("discord.js");
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
 });
-const axios = require("axios");
-const { JSONParser } = require("@streamparser/json");
 const { EmbedBuilder } = require("discord.js");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_PRO_KEY);
 
 client.on("ready", () => {
   console.log(`${client.user.tag} has logged in.`);
 });
 
+/** @type {{ role: string, parts: string }[]} */
+const conversations = [];
+
 client.on("messageCreate", async (message) => {
   if (message.author.bot) return;
   if (message.channel?.topic?.includes("Gemini")) {
     if (message.content.startsWith("#")) return;
+    if (message.content.startsWith("clear")) {
+      conversations.length = 0;
+      message.reply("Clear!");
+      return;
+    }
     const content = message.cleanContent.trim();
     const msg = await message.reply("Generating...");
-    const res = await axios.post(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:streamGenerateContent?key=${process.env.GEMINI_PRO_KEY}`,
-      { contents: [{ parts: [{ text: content }] }] },
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        responseType: "stream",
-      }
-    );
-    const parser = new JSONParser();
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    const chat = await model.startChat({ history: conversations });
+    const res = await chat.sendMessageStream(content);
     let text = "";
-    parser.onValue = ({ value }) => {
-      if (value.text) {
-        text += value.text;
+    let ended = false;
+    (async () => {
+      for await (const chunk of res.stream) {
+        const chunkText = chunk.text();
+        console.log(chunkText);
+        text += chunkText;
       }
-    };
-
-    res.data.on("data", (chunk) => {
-      parser.write(chunk.toString());
-    });
+      ended = true;
+    })();
 
     const to = setInterval(() => {
-      msg.edit(text.slice(0,1999));
+      msg.edit(text.slice(0, 1999));
     }, 3000);
 
-    res.data.on("end", () => {
-      const embed = new EmbedBuilder()
-        .setDescription("2000文字を超えるため、一部のみ表示しています。")
-      msg.edit({ content: text.slice(0,1999), embeds: [text.length > 1999 ? embed:[]].flat() });
-      clearInterval(to);
+    await new Promise((resolve) => {
+      const interval = setInterval(() => {
+        if (ended) {
+          clearInterval(interval);
+          resolve();
+        }
+      }, 1000);
     });
+    const embed = new EmbedBuilder()
+      .setDescription("2000文字を超えるため、一部のみ表示しています。")
+    msg.edit({ content: text.slice(0, 1999), embeds: [text.length > 1999 ? embed : []].flat() });
+    clearInterval(to);
+    conversations.push({ role: "user", parts: content });
+    conversations.push({ role: "model", parts: text });
   }
 });
 
